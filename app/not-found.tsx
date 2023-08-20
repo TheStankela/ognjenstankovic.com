5f120f0baa5ac28aab80be3d5ed19c93a72fe592
TheStankela <ognjenogistankovic2002@gmail.com>
2023-08-20 17:36:16 +0200

    Bugfixes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,8 +8,8 @@ export default function NotFound(){
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-sm text-center">
                     <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-white">404</h1>
-                    <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">Something's missing.</p>
-                    <p className="mb-4 text-lg font-light  text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                    <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">Something&apos;s missing.</p>
+                    <p className="mb-4 text-lg font-light  text-gray-400">Sorry, we can&apos;t find that page. You&apos;ll find lots to explore on the home page. </p>
                     <Link href="/" className="inline-flex text-white bg-gray-800 hover:bg_dark_blue
                     focus:ring-4 focus:outline-none font-medium rounded-lg 
                     text-sm px-5 py-2.5 text-center focus:ring-primary-900 my-4">Back to Homepage</Link>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
@@ -7,7 +8,12 @@ const Footer = () => {
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <Link href="/" className="flex items-center mb-4 sm:mb-0">
-                <img src="/images/avatar.png" className="h-8 mr-3" alt="Flowbite Logo" />
+                <Image 
+                src="/images/avatar.png"
+                className="h-8 mr-3" 
+                height={32}
+                width={32}
+                alt="Flowbite Logo" />
                 <span className="self-center text-xl font-semibold whitespace-nowrap text-white">Ognjen Stanković</span>
             </Link>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 text-gray-400">
