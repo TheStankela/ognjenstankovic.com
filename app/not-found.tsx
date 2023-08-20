f97326389770d4efc646e095bb6e004701290dec
TheStankela <ognjenogistankovic2002@gmail.com>
2023-08-20 17:09:00 +0200

    Added notfound page

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react'
+
+
+export default function NotFound(){
+    return (
+        <section className="py-24 bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div className="mx-auto max-w-screen-sm text-center">
+                    <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-white">404</h1>
+                    <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">Something's missing.</p>
+                    <p className="mb-4 text-lg font-light  text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                    <Link href="/" className="inline-flex text-white bg-gray-800 hover:bg_dark_blue
+                    focus:ring-4 focus:outline-none font-medium rounded-lg 
+                    text-sm px-5 py-2.5 text-center focus:ring-primary-900 my-4">Back to Homepage</Link>
+                </div>   
+             </div>
+        </section>
+    );
+}
