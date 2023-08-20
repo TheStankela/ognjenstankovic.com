99246f43de0f23e064f70bb4cff3509240570229
TheStankela <ognjenogistankovic2002@gmail.com>
2023-08-20 17:32:05 +0200

    Icon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: "Ognjen Stanković | New Tech Blog!",
   description: 'Covering every topic of the tech world!',
+  icons: {
+    icon: "favicon.ico"
+  }
 }
 
 export default function RootLayout({
