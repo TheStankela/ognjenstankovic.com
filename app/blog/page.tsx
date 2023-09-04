830d4309fb0226946b0cd991fada0e6123523e52
Ognjen Stankovic <121059343+TheStankela@users.noreply.github.com>
2023-09-04 18:13:39 +0200

    Added key

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -22,7 +22,7 @@ export default async function Blog() {
             </div> 
                 <div className="grid gap-8 max-w-screen-md mx-auto">
                    {posts.map(post => (
-                    <article className="p-6 rounded-lg border  shadow-md bg-gray-800 border-gray-700 ">
+                    <article key={post.id} className="p-6 rounded-lg border  shadow-md bg-gray-800 border-gray-700 ">
                         <div className="flex justify-between items-center mb-5 text-gray-500">
                             <span className="text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded bg-primary-200 text-primary-800">
                                 <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
