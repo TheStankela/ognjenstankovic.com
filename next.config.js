127c653ff3501ebaf026244fe664fdcf7a14c444
Ognjen Stankovic <121059343+TheStankela@users.noreply.github.com>
2023-09-08 17:54:05 +0200

    Appdir: true

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    appDir: true,
+    experimental: {
+        appDir: true,
+      },
     reactStrictMode: true,
     images:{
         remotePatterns: [
